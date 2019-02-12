b3d98e734297108233aed0a93086adb2e0c3f1cb
Christian Valencia Gutierrez <christian.valencia@globant.com>
2019-02-12 17:22:41 -0500

    initial commit

---
+++ src/main/java/com/partnerships/interview/app/InterviewApplication.java
@@ -0,0 +1,35 @@
+package com.partnerships.interview.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Date;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.vivid.partnerships.interview"})
+public class InterviewApplication implements CommandLineRunner {
+	private static final Logger LOGGER = LoggerFactory.getLogger(InterviewApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewApplication.class, args);
+	}
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	@Override
+	public void run(String... strings) throws Exception {
+		LOGGER.info("Creating events table");
+
+		jdbcTemplate.execute("CREATE TABLE events(" +
+				"id SERIAL, name VARCHAR(255), date DATE)");
+
+		jdbcTemplate.update("INSERT INTO events(name, date) VALUES (?,?)", "Chicago White Sox vs. Chicago Cubs", new Date());
+	}
+}
---
+++ src/main/java/com/partnerships/interview/model/Event.java
@@ -0,0 +1,49 @@
+package com.partnerships.interview.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity(name = "event")
+public class Event {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private Date date;
+    private Venue venue;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public void setVenue(Venue venue) {
+        this.venue = venue;
+    }
+}
